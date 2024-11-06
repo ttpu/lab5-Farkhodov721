ee7d714cfad3600822cf741171f4c783641b7068
Farkhodov Abdulazizbek <116057829+Farkhodov721@users.noreply.github.com>
2024-11-06 06:36:39 +0000

    finished all exercise

--- ex5/main.c
+++ ex5/main.c
@@ -1,7 +1,11 @@
-
 #include <stdio.h>
+int main() {
+    printf("Letter  ASCII  Capital Letter  ASCII\n");
+    printf("----------------------------------------\n");
 
-int main(void)
-{
-    printf("Hello World\n");
+    for (char lowercase = 'a', uppercase = 'A'; lowercase <= 'z'; lowercase++, uppercase++) {
+        printf("%c      %d        %c     %d\n", lowercase, (int)lowercase,uppercase, (int)uppercase);
+    }
+    return 0;
 }
+
